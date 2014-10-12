9bb10062d6c88b0c3bec1e0b354f89da38fecbe9
Felipe Macias <felipe.macias@gmail.com>
2014-10-12 12:08:42 -0500

    commit, names on plot1.R

--- plot1.R
+++ plot1.R
@@ -4,8 +4,10 @@
 
 datos <- read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'),header=FALSE,sep=";",colClasses=c("character", "character", rep("numeric",7)),na="?")
 
-# Since there is no header in the data loaded the xlab label was given
-hist(datos[,3],col="red",xlab="Global Active Power (kilowatts)",main = "Global Active Power")
+# Names are restaured
+names(datos) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+hist(datos$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",main = "Global Active Power")
 
 # command for sending the plot to plot1.png
 
